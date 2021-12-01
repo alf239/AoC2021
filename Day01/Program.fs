a12740f81d6610a1328bafb43b992309ae2b0a81
Alexey Filippov <alexey@filippov.uk>
2021-12-01 05:26:21 +0000

    Day 1 - cleaner code

--- Day01/Program.fs
+++ Day01/Program.fs
@@ -2,14 +2,20 @@ open System.IO
 
 [<EntryPoint>]
 let main args =
-    let input = File.ReadLines args.[0] |> Seq.map int 
+    let input = File.ReadLines args.[0] |> Seq.map int
 
-    let dx = input |> Seq.windowed 2
-    let increases = dx |> Seq.filter (fun a -> a.[1] > a.[0]) |> Seq.toList
-    printfn $"Hello from day 1.1: {List.length increases}"
+    let increases =
+        Seq.windowed 2
+        >> Seq.filter (fun a -> a.[1] > a.[0])
+        >> Seq.length
 
-    let dx = input |> Seq.windowed 3 |> Seq.map Array.sum |> Seq.toList
-    let increases = dx |> Seq.windowed 2 |> Seq.filter (fun a -> a.[1] > a.[0]) |> Seq.toList
-    printfn $"Hello from day 1.2: {List.length increases}"
-    
+    printfn $"Day 1.1: {increases input}"
+
+    let dx =
+        input
+        |> Seq.windowed 3
+        |> Seq.map Array.sum
+
+    printfn $"Day 1.2: {increases dx}"
+
     0
