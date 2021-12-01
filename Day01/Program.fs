e6af1cdac3cee9c8e5e906b4985fb17780f6d00d
Alexey Filippov <alexey@filippov.uk>
2021-12-01 05:10:07 +0000

    Day 1: Pass

--- Day01/Program.fs
+++ Day01/Program.fs
@@ -1,2 +1,15 @@
+open System.IO
 
-printfn "Hello from day 1"
+[<EntryPoint>]
+let main args =
+    let input = File.ReadLines args.[0] |> Seq.map int 
+
+    let dx = input |> Seq.windowed 2
+    let increases = dx |> Seq.filter (fun a -> a.[1] > a.[0]) |> Seq.toList
+    printfn $"Hello from day 1.1: {List.length increases}"
+
+    let dx = input |> Seq.windowed 3 |> Seq.map Array.sum |> Seq.toList
+    let increases = dx |> Seq.windowed 2 |> Seq.filter (fun a -> a.[1] > a.[0]) |> Seq.toList
+    printfn $"Hello from day 1.2: {List.length increases}"
+    
+    0
