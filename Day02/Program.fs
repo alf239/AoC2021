5547dfc93ac6fbd267cd3b4f2ed3c46bdf429096
Alexey Filippov <alexey@filippov.uk>
2021-12-02 06:43:10 +0000

    Day 2 - submission

--- Day02/Program.fs
+++ Day02/Program.fs
@@ -2,6 +2,31 @@ open AoC.Magic
 
 let input = taskInput 2021 2 |> asStrings
 
-let increases = fun xs -> "hi"
+let commands =
+    input
+    |> Seq.map
+        (fun s ->
+            let parts = s.Split(' ')
+            parts.[0], int parts.[1])
 
-printfn $"Day 2.1: {increases input}"
+let x, y =
+    commands
+    |> Seq.fold
+        (fun (x, y) cmd ->
+            match cmd with
+            | "forward", n -> x + n, y
+            | "down", n -> x, y + n
+            | "up", n -> x, y - n) (0, 0)
+
+printfn $"Day 2.1: {x * y}"
+
+let x2, y2, aim =
+    commands
+    |> Seq.fold
+        (fun (x, y, aim) cmd ->
+            match cmd with
+            | "forward", n -> x + n, y + aim * n, aim
+            | "down", n -> x, y, aim + n
+            | "up", n -> x, y, aim - n) (0, 0, 0)
+
+printfn $"Day 2.2: {x2 * y2}"
