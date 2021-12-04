09c1cc8edcec83465f4f1429d5933811a50a5b14
Alexey Filippov <alexey@filippov.uk>
2021-12-04 07:04:28 +0000

    Day 5 - boilerplate

---
+++ Day05/Program.fs
@@ -0,0 +1,11 @@
+open AoC.Magic
+
+let forReal = true
+let rawInput = if forReal then taskInput 2021 4 else ""
+let input = rawInput |> nonEmptyLines |> Seq.toList
+
+let task1 ss = -1
+let task2 ss = -2
+
+printfn $"Day 5.1: {task1 input}"
+printfn $"Day 5.2: {task2 input}"
