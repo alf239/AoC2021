5c5c3ab30170fcdf14b688b3984c7635ec423565
Alexey Filippov <alexey@filippov.uk>
2021-12-06 20:42:02 +0000

    Day 7 - boilerplate

---
+++ Day07/Program.fs
@@ -0,0 +1,20 @@
+open AoC.Magic
+
+let realInput = taskInput 2021 6
+let testInput = "3,4,3,1,2"
+let parse = nonEmptyLines >> Seq.toList
+
+let task1 data =
+    -1
+
+let task2 data =
+    -2
+
+let fullTask1 = parse >> task1
+let fullTask2 = parse >> task2
+
+assert (testInput |> parse |> task1 = -1)
+assert (testInput |> parse |> task2 = -2)
+
+printfn $"Day 7.1: {realInput |> fullTask1}"
+printfn $"Day 7.2: {realInput |> fullTask2}"
