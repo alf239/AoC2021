b8d88e7f93cf890b2ca0d4d21d93a05b722494a5
Alexey Filippov <alexey@filippov.uk>
2021-12-09 05:42:28 +0000

    Day 9 - task 2

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -1,3 +1,4 @@
+open System.Collections.Generic
 open AoC.Magic
 
 let realInput = taskInput 2021 9
@@ -10,7 +11,7 @@ let testInput =
 9899965678"
 
 let testAnswer1 = 15
-let testAnswer2 = -2
+let testAnswer2 = 1134
 
 let parse =
     nonEmptyLines
@@ -19,7 +20,7 @@ let parse =
     >> Seq.map (Array.map (fun x -> x - (int '0')))
     >> Array.ofSeq
 
-let risk (data: int [] []) x y =
+let isLowest (data: int [] []) x y =
     let u = if y = 0 then 10 else data.[x].[y - 1]
 
     let d =
@@ -37,21 +38,52 @@ let risk (data: int [] []) x y =
     let l = if x = 0 then 10 else data.[x - 1].[y]
     let v = data.[x].[y]
 
-    if v < u && v < d && v < r && v < l then
-        v + 1
-    else
-        0
+    let lowest = v < u && v < d && v < r && v < l
+    lowest
 
-let task1 data =
+let task data fn =
     seq {
         for y in 0 .. Array.length (Array.head data) - 1 do
             for x in 0 .. Array.length data - 1 do
-                yield risk data x y
-
+                if isLowest data x y then
+                    yield fn data x y
     }
-    |> Seq.sum
 
-let task2 data = -2
+let dfs (data: int [] []) x y =
+    let seen = HashSet()
+    let work = Queue()
+    work.Enqueue(x, y)
+    let mutable cnt = 0
+
+    while work.Count > 0 do
+        let a, b = work.Dequeue()
+
+        if not <| seen.Contains((a, b)) then
+            seen.Add((a, b)) |> ignore
+
+            let border =
+                (a = -1)
+                || (b = -1)
+                || (a = Array.length data)
+                || (b = Array.length data.[0])
+                || (data.[a].[b] = 9)
+
+            if not border then
+                cnt <- cnt + 1
+
+                work.Enqueue((a - 1, b))
+                work.Enqueue((a, b - 1))
+                work.Enqueue((a + 1, b))
+                work.Enqueue((a, b + 1))
+
+    cnt
+
+
+let task1 data = task data (fun d x y -> d.[x].[y] + 1) |> Seq.sum
+
+let task2 data = 
+    let sizes = task data dfs
+    sizes |> Seq.sort |> Seq.rev |> Seq.take 3 |> Seq.fold (fun acc x -> acc * x) 1
 
 let fullTask1 = parse >> task1
 let fullTask2 = parse >> task2
