57a49131a97f57a682ea7bbfe06c890dd8ed6669
Alexey Filippov <alexey@filippov.uk>
2021-12-09 05:14:44 +0000

    Day 9 - task 1

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -2,14 +2,54 @@ open AoC.Magic
 
 let realInput = taskInput 2021 9
 
-let testInput = ""
+let testInput =
+    "2199943210\n\
+3987894921\n\
+9856789892\n\
+8767896789\n\
+9899965678"
 
-let testAnswer1 = -1
+let testAnswer1 = 15
 let testAnswer2 = -2
 
-let parse = nonEmptyLines
+let parse =
+    nonEmptyLines
+    >> Seq.map (fun (s: string) -> s.ToCharArray())
+    >> Seq.map (Array.map int)
+    >> Seq.map (Array.map (fun x -> x - (int '0')))
+    >> Array.ofSeq
 
-let task1 data = -1
+let risk (data: int [] []) x y =
+    let u = if y = 0 then 10 else data.[x].[y - 1]
+
+    let d =
+        if y = Array.length (Array.head data) - 1 then
+            10
+        else
+            data.[x].[y + 1]
+
+    let r =
+        if x = Array.length data - 1 then
+            10
+        else
+            data.[x + 1].[y]
+
+    let l = if x = 0 then 10 else data.[x - 1].[y]
+    let v = data.[x].[y]
+
+    if v < u && v < d && v < r && v < l then
+        v + 1
+    else
+        0
+
+let task1 data =
+    seq {
+        for y in 0 .. Array.length (Array.head data) - 1 do
+            for x in 0 .. Array.length data - 1 do
+                yield risk data x y
+
+    }
+    |> Seq.sum
 
 let task2 data = -2
 
