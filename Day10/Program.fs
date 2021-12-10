d4aed153c68dc141e67cfceb8d8009274e9a7cb2
Alexey Filippov <alexey@filippov.uk>
2021-12-10 05:32:26 +0000

    Day 10 - submission

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -1,17 +1,99 @@
+open System.Collections.Generic
 open AoC.Magic
 
 let realInput = taskInput 2021 10
 
-let testInput = ""
+let testInput =
+    "[({(<(())[]>[[{[]{<()<>>\n\
+[(()[<>])]({[<{<<[]>>(\n\
+{([(<{}[<>[]}>{[]{[(<()>\n\
+(((({<>}<{<{<>}{[]{[]{}\n\
+[[<[([]))<([[{}[[()]]]\n\
+[{[{({}]{}}([{[{{{}}([]\n\
+{<[[]]>}<{[{[{[]{()[[[]\n\
+[<(<(<(<{}))><([]([]()\n\
+<{([([[(<>()){}]>(<<{{\n\
+<{([{{}}[<[[[<>{}]]]>[]]"
 
-let testAnswer1 = -1
-let testAnswer2 = -2
+let testAnswer1 = 26397L
+
+let testAnswer2 = 288957L
 
 let parse = nonEmptyLines
 
-let task1 data = -1
+let errorScore (s: string): int64 =
+    let stack = Stack()
+    let mutable penalty: int64 = 0L
 
-let task2 data = -2
+    for c in s do
+        match c with
+        | '('
+        | '['
+        | '{'
+        | '<' -> stack.Push c
+        | ')' ->
+            let c1 = stack.Pop()
+
+            if '(' <> c1 && penalty = 0L then
+                penalty <- 3L
+        | ']' ->
+            let c1 = stack.Pop()
+
+            if '[' <> c1 && penalty = 0L then
+                penalty <- 57L
+        | '}' ->
+            let c1 = stack.Pop()
+
+            if '{' <> c1 && penalty = 0L then
+                penalty <- 1197L
+        | '>' ->
+            let c1 = stack.Pop()
+
+            if '<' <> c1 && penalty = 0L then
+                penalty <- 25137L
+
+    penalty
+
+
+let completionScore s =
+    let cs =
+        function
+        | '(' -> 1L
+        | '[' -> 2L
+        | '{' -> 3L
+        | '<' -> 4L
+
+    let stack = Stack()
+
+    for c in s do
+        match c with
+        | '('
+        | '['
+        | '{'
+        | '<' -> stack.Push c
+        | ')'
+        | ']'
+        | '}'
+        | '>' -> stack.Pop() |> ignore
+
+    let rest =
+        seq {
+            while stack.Count > 0 do
+                yield stack.Pop()
+        }
+
+    Seq.fold (fun acc x -> acc * 5L + cs x) 0L rest
+
+let median xs =
+    let sorted = xs |> Array.ofSeq |> Array.sort
+    sorted.[Array.length sorted / 2]
+
+let task1 = Seq.map errorScore >> Seq.sum
+
+let task2 =
+    Seq.filter (fun s -> errorScore s = 0L)
+    >> Seq.map completionScore
+    >> median
 
 let fullTask1 = parse >> task1
 let fullTask2 = parse >> task2
