70f2ad1b5f4c370b9d2fb5828c6796c4951e4a5f
Alexey Filippov <alexey@filippov.uk>
2021-12-14 05:51:45 +0000

    Day 14 - task 2

--- Day14/Program.fs
+++ Day14/Program.fs
@@ -22,12 +22,19 @@ BC -> B\n\
 CC -> N\n\
 CN -> C"
 
-let testAnswer1 = 1588
-let testAnswer2 = -2
+let testAnswer1 = 1588L
+let testAnswer2 = 2188189693529L
 
 let parse s =
     let ls = nonEmptyLines s
-    let poly = ls |> Seq.head |> Array.ofSeq
+    let poly = ls |> Seq.head
+
+    let preparedPoly =
+        poly |> Seq.last,
+        poly
+        |> Seq.windowed 2
+        |> Seq.countBy id
+        |> Seq.map (fun (k, cnt) -> k, int64 cnt)
 
     let rules =
         ls
@@ -35,35 +42,47 @@ let parse s =
         |> Seq.map
             (fun s ->
                 let [| pair; _; insert |] = words s
-                pair |> Array.ofSeq, insert |> Seq.head)
+                let [| c1; c2 |] = pair |> Array.ofSeq
+                let insert = insert |> Seq.head
+                [| c1; c2 |], [ [| c1; insert |]; [| insert; c2 |] ])
         |> Map.ofSeq
 
-    poly, rules
+    preparedPoly, rules
 
-let step (poly: char seq) (rules: Map<char [], char>) =
-    let last = poly |> Seq.last
+let step (poly: char * seq<char [] * int64>) (rules: Map<char [], #seq<char []>>) =
+    let last, pairs = poly
 
-    let newPoly =
-        poly
-        |> Seq.windowed 2
+    let newPairs =
+        pairs
         |> Seq.collect
-            (fun pair ->
-                let insert = rules |> Map.find pair
-                [ pair |> Seq.head; insert ])
+            (fun (pair, cnt) ->
+                rules
+                |> Map.find pair
+                |> Seq.map (fun k -> k, int64 cnt))
+        |> Seq.groupBy fst
+        |> Seq.map (fun (k, cnts) -> k, cnts |> Seq.map snd |> Seq.sum)
 
-    Seq.append newPoly [ last ]
+    last, newPairs
 
-let task1 (poly, rules) =
-    let poly10 =
-        [ 1 .. 10 ]
+let task (poly, rules) n =
+    let last, pairs =
+        [ 1 .. n ]
         |> Seq.fold (fun acc _ -> step acc rules) poly
 
-    let frequencies = poly10 |> Seq.countBy id
+    let frequencies =
+        pairs
+        |> Seq.map (fun (k, cnt) -> k |> Seq.head, cnt)
+        |> Seq.append [ (last, 1L) ]
+        |> Seq.groupBy fst
+        |> Seq.map (fun (k, cnts) -> k, cnts |> Seq.map snd |> Seq.sum)
+
     let mx = frequencies |> Seq.maxBy snd |> snd
     let mn = frequencies |> Seq.minBy snd |> snd
     mx - mn
 
-let task2 data = -2
+let task1 data = task data 10
+
+let task2 data = task data 40
 
 let fullTask1 = parse >> task1
 let fullTask2 = parse >> task2
