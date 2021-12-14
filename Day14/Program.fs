9e671620613e8a35282ec6e168552cd4ef94ebe1
Alexey Filippov <alexey@filippov.uk>
2021-12-14 07:11:09 +0000

    Day 14 - well that's embarrassing

--- Day14/Program.fs
+++ Day14/Program.fs
@@ -3,23 +3,23 @@ open AoC.Magic
 let realInput = taskInput 2021 14
 
 let testInput =
-    "NNCB\n\
-\n\
-CH -> B\n\
-HH -> N\n\
-CB -> H\n\
-NH -> C\n\
-HB -> C\n\
-HC -> B\n\
-HN -> C\n\
-NN -> C\n\
-BH -> H\n\
-NC -> B\n\
-NB -> B\n\
-BN -> B\n\
-BB -> N\n\
-BC -> B\n\
-CC -> N\n\
+    "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
 CN -> C"
 
 let testAnswer1 = 1588L
