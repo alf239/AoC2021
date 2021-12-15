7e3fc1dd5e51625f0ce918cacc3cd381c89ddedb
Alexey Filippov <alexey@filippov.uk>
2021-12-15 05:35:35 +0000

    Day 15 - pass the example, not accepted

--- Day15/Program.fs
+++ Day15/Program.fs
@@ -2,14 +2,56 @@ open AoC.Magic
 
 let realInput = taskInput 2021 15
 
-let testInput = ""
+let testInput =
+    "\
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581"
 
-let testAnswer1 = -1
+let testAnswer1 = 40
 let testAnswer2 = -2
 
-let parse = nonEmptyLines
+let charToint (s: string) =
+    s.ToCharArray()
+    |> Array.map (fun c -> int c - int '0')
 
-let task1 data = -1
+let parse =
+    nonEmptyLines >> Seq.map charToint >> Array.ofSeq
+
+let format data =
+    data
+    |> Seq.map (Array.map <| sprintf "%4d" >> String.concat " ")
+    |> String.concat "\n"
+
+let task1 data =
+    let H = data |> Array.length
+    let W = data.[0] |> Array.length
+
+    let dp =
+        [| for _y in 0 .. H - 1 -> [| for _x in 0 .. W - 1 -> 0 |] |]
+
+    for x in 1 .. W - 1 do
+        dp.[0].[x] <- dp.[0].[x - 1] + data.[0].[x]
+
+    for y in 1 .. H - 1 do
+        dp.[y].[0] <- dp.[y - 1].[0] + data.[y].[0]
+
+    for y in 1 .. H - 1 do
+        for x in 1 .. W - 1 do
+            dp.[y].[x] <- (min dp.[y - 1].[x] dp.[y].[x - 1]) + data.[y].[x]
+
+//    printf "%s" <| format data
+//    printf "\n\n"
+//    printf "%s" <| format dp
+
+    dp.[H - 1].[W - 1]
 
 let task2 data = -2
 
