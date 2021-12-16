0eb97464d5d8db7dda91c21318c8cc478f55ad1d
Alexey Filippov <alexey@filippov.uk>
2021-12-16 07:39:11 +0000

    Day 16 - task 2

--- Day16/Program.fs
+++ Day16/Program.fs
@@ -2,21 +2,24 @@ open AoC.Magic
 
 let realInput = taskInput 2021 16
 
-let tests =
-    [ "D2FE28", 6, -2
-      "EE00D40C823060", 14, -2
-      "8A004A801A8002F478", 16, -2
-      "620080001611562C8802118E34", 12, -2
-      "C0015000016115A2E0802F182340", 23, -2
-      "A0016C880162017C3686B18A3D4780", 31, -2 ]
+let tests1 =
+    [ "D2FE28", 6
+      "EE00D40C823060", 14
+      "8A004A801A8002F478", 16
+      "620080001611562C8802118E34", 12
+      "C0015000016115A2E0802F182340", 23
+      "A0016C880162017C3686B18A3D4780", 31 ]
 
-let parse (s: string) =
-    s.Trim()
-    |> Seq.map
-        (function
-        | x when x >= '0' && x <= '9' -> byte x - byte '0'
-        | x when x >= 'A' && x <= 'F' -> byte x - byte 'A' + 10uy)
-    |> Array.ofSeq
+let tests2 =
+    [ "D2FE28", 2021
+      "C200B40A82", 3
+      "04005AC33890", 54
+      "880086C3E88112", 7
+      "CE00C43D881120", 9
+      "D8005AC2A8F0", 1
+      "F600BC2D8F", 0
+      "9C005AC2F8F0", 0
+      "9C0141080250320F1802104A08", 1 ]
 
 let rec consume nr (data: byte []) (pos: int) : uint64 =
     let shift = pos % 4
@@ -33,9 +36,28 @@ let rec consume nr (data: byte []) (pos: int) : uint64 =
         let lo = consume remaining data (pos + hbits)
         (hi <<< remaining) ||| lo
 
+type Expression =
+    | Sum
+    | Product
+    | Minimum
+    | Maximum
+    | GreaterThan
+    | LessThan
+    | EqualTo
+
 type Packet =
     | Literal of byte * uint64
-    | Operator of byte * byte * Packet list
+    | Operator of byte * Expression * Packet list
+
+let expr typ =
+    match int typ with
+    | 0 -> Sum
+    | 1 -> Product
+    | 2 -> Minimum
+    | 3 -> Maximum
+    | 5 -> GreaterThan
+    | 6 -> LessThan
+    | 7 -> EqualTo
 
 let rec readPackets data pos length =
     let mutable p = pos
@@ -76,7 +98,7 @@ and firstPacket data pos =
             let length = consume 15 data (pos + 7) |> int
             let packets, p = readPackets data (pos + 7 + 15) length
 
-            Operator(version, ptype |> byte, packets), p
+            Operator(version, ptype |> expr, packets), p
         else
             let length = consume 11 data (pos + 7) |> int
             let mutable p = pos + 7 + 11
@@ -90,7 +112,40 @@ and firstPacket data pos =
                 }
                 |> List.ofSeq
 
-            Operator(version, ptype |> byte, packets), p
+            Operator(version, ptype |> expr, packets), p
+
+let parse (s: string) =
+    let data =
+        s.Trim()
+        |> Seq.map
+            (function
+            | x when x >= '0' && x <= '9' -> byte x - byte '0'
+            | x when x >= 'A' && x <= 'F' -> byte x - byte 'A' + 10uy)
+        |> Array.ofSeq
+
+    firstPacket data 0 |> fst
+
+
+let rec eval packet =
+    match packet with
+    | Literal (_, x) -> x
+    | Operator (_, expr, packets) ->
+        let terms = packets |> List.map eval
+
+        match expr with
+        | Sum -> Seq.sum terms
+        | Product -> Seq.fold (*) 1UL terms
+        | Minimum -> Seq.min terms
+        | Maximum -> Seq.max terms
+        | GreaterThan ->
+            terms
+            |> (fun [ a; b ] -> if a > b then 1UL else 0UL)
+        | LessThan ->
+            terms
+            |> (fun [ a; b ] -> if a < b then 1UL else 0UL)
+        | EqualTo ->
+            terms
+            |> (fun [ a; b ] -> if a = b then 1UL else 0UL)
 
 let rec sumOfVersions p =
     match p with
@@ -99,32 +154,18 @@ let rec sumOfVersions p =
         (version |> int)
         + (packets |> Seq.map sumOfVersions |> Seq.sum)
 
-let task1 data =
-    let packet, _ = firstPacket data 0
-    sumOfVersions packet
+let task1 = sumOfVersions >> uint64
 
-let task2 data = -2
+let task2 = eval
 
 let fullTask1 = parse >> task1
 let fullTask2 = parse >> task2
 
-let results1 =
-    tests
-    |> Seq.map (fun (t, _, _) -> fullTask1 t)
-    |> List.ofSeq
+let results1 = tests1 |> List.map (fst >> fullTask1)
+assert (results1 = (tests1 |> List.map (snd >> uint64)))
 
-assert (results1 = (tests
-                    |> Seq.map (fun (_, r, _) -> r)
-                    |> List.ofSeq))
-
-let results2 =
-    tests
-    |> Seq.map (fun (t, _, _) -> fullTask2 t)
-    |> List.ofSeq
-
-assert (results2 = (tests
-                    |> Seq.map (fun (_, _, r) -> r)
-                    |> List.ofSeq))
+let results2 = tests2 |> List.map (fst >> fullTask2)
+assert (results2 = (tests2 |> List.map (snd >> uint64)))
 
 printfn $"Day 16.1: {realInput |> fullTask1}"
 printfn $"Day 16.2: {realInput |> fullTask2}"
