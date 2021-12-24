aa38d66fc4309aaab140f27216a369dfcac0c811
Alexey Filippov <alexey@filippov.uk>
2021-12-24 16:38:17 +0000

    Day 24 - manually, why not

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -2,20 +2,59 @@ open AoC.Magic
 
 let parse = nonEmptyLines
 
-let task1 data = -1
+let step z a b c w =
+    if z = [] || ((w - b) <> (z |> List.head)) then
+        assert (a = 1)
+        (w + c) :: z
+    else
+        assert (a = 26)
+        z |> List.tail
 
-let task2 data = -2
+let program (input: int64) =
+    let ds =
+        input
+        |> string
+        |> Seq.map (fun c -> int c - int '0')
+
+    let prg =
+        [ 1, 15, 9
+          1, 11, 1
+          1, 10, 11
+          1, 12, 3
+          26, -11, 10
+          1, 11, 5
+          1, 14, 0
+          26, -6, 7
+          1, 10, 9
+          26, -6, 15
+          26, -6, 4
+          26, -16, 10
+          26, -4, 4
+          26, -2, 9 ]
+
+    let steps =
+        ds
+        |> Seq.zip prg
+        |> Seq.scan (fun z ((a, b, c), w) -> step z a b c w) []
+        |> List.ofSeq
+
+    steps |> List.last |> List.length
+
+let task1 _ =
+    let guess = 29991993698469L
+    let z = program guess
+    assert (z = 0)
+    guess
+
+let task2 _ =
+    let guess = 14691271141118L
+    let z = program guess
+    assert (z = 0)
+    guess
+
 
 let fullTask1 = parse >> task1
 let fullTask2 = parse >> task2
-
-let testInput = ""
-let testAnswer1 = -1
-let testAnswer2 = -2
-let result1 = testInput |> fullTask1
-assert (result1 = testAnswer1)
-let result2 = testInput |> fullTask2
-assert (result2 = testAnswer2)
 
 let realInput = taskInput 2021 24
 printfn $"Day 24.1: {realInput |> fullTask1}"
